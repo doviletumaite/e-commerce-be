c1a4720d576dad696446abb80a74ecc786e9a6fd
Dovile <dovile.tumaite@yahoo.it>
2025-09-18 19:16:41 +0200

    wip: added search

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -5,6 +5,7 @@ namespace Core.Specifications;
 public class ProductSpecification : BaseSpecification<Product>
 {
     public ProductSpecification(ProductSpecParams specParms) : base(x =>
+        (string.IsNullOrEmpty(specParms.Search) || x.Name.ToLower().Contains(specParms.Search)) &&
         (specParms.Brands.Count == 0 || specParms.Brands.Contains(x.Brand)) &&
         (specParms.Types.Count == 0 || specParms.Types.Contains(x.Type))
     )
