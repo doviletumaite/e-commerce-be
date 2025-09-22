0fe76f303a2464641a16e4d2fdf4324e17d6f373
Dovile <dovile.tumaite@yahoo.it>
2025-09-22 15:54:05 +0200

    wip: added validation error response

---
+++ API/DTOs/CreateProductDto.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class CreateProductDto
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public string Description { get; set; } = string.Empty;
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greter than 0")]
+    public decimal Price { get; set; }
+
+    [Required]
+    public string PictureId { get; set; } = string.Empty;
+
+    [Required]
+    public string PictureUrl { get; set; } = string.Empty;
+
+    [Required]
+    public string Type { get; set; } = string.Empty;
+
+    [Required]
+    public string Brand { get; set; } = string.Empty;
+
+    [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 1")]
+    public int QuantityInStock { get; set; }
+
+}
