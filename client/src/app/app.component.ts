4cb3d64abc7c5e59497fdcabfce5c92b40682b69
Dovile <dovile.tumaite@yahoo.it>
2025-11-14 15:48:17 +0100

    wip: fe start

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = signal('client');
+}
